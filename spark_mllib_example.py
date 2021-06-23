36f35f9a9a7a138a2b99f34f575342d8d93e53c7
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-06-23 08:00:20 +0400

    Add files via upload

--- spark_mllib_example.py
+++ spark_mllib_example.py
@@ -8,6 +8,7 @@ https://spark.apache.org/docs/latest/ml-classification-regression.html
 '''
 build the spark envirenment 
 '''
+
 import pyspark
 from pyspark import *
 from pyspark.sql import *
@@ -16,7 +17,6 @@ from pyspark.sql.functions import *
 
 sc = SparkContext("local")
 spark = SparkSession.builder.getOrCreate()
-
 
 ##########
 
@@ -254,3 +254,60 @@ pca = PCA(k=2,
 model = pca.fit(df)
 result = model.transform(df)
 result.show(truncate=False)
+
+
+
+#################
+
+
+'''
+if you have records of buyers buying products, and you want to recommend new product to buyers, 
+
+use recommendation
+
+'''
+
+
+'''
+wget https://raw.githubusercontent.com/apache/spark/master/data/mllib/als/sample_movielens_ratings.txt
+'''
+
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.recommendation import ALS
+from pyspark.sql import Row
+
+lines = spark.read.text("sample_movielens_ratings.txt").rdd
+parts = lines.map(lambda row: row.value.split("::"))
+ratingsRDD = parts.map(lambda p: 
+	Row(userId=int(p[0]), 
+	movieId=int(p[1]),
+	rating=float(p[2]), 
+	timestamp=int(p[3])))
+ratings = spark.createDataFrame(ratingsRDD)
+(training, test) = ratings.randomSplit([0.8, 0.2])
+
+als = ALS(maxIter=5, regParam=0.01, userCol="userId", itemCol="movieId", ratingCol="rating",
+          coldStartStrategy="drop")
+model = als.fit(training)
+
+
+predictions = model.transform(test)
+
+evaluator = RegressionEvaluator(
+	metricName="rmse", 
+	labelCol="rating",
+	predictionCol="prediction")
+rmse = evaluator.evaluate(predictions)
+print("Root-mean-square error = " + str(rmse))
+
+
+userRecs = model.recommendForAllUsers(10)
+movieRecs = model.recommendForAllItems(10)
+
+
+users = ratings.select(als.getUserCol()).distinct().limit(3)
+userSubsetRecs = model.recommendForUserSubset(users, 10)
+
+
+movies = ratings.select(als.getItemCol()).distinct().limit(3)
+movieSubSetRecs = model.recommendForItemSubset(movies, 10)
